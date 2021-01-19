a1fd0af07d1a0372dd267ae8987361912700431c
Forrest Goyer <forrest.goyer@cerner.com>
2021-01-18 18:02:56 -0600

    First implementation of evernoise genre scrape

--- every-genre.go
+++ every-genre.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+var (
+	genres []string
 )
 
 func main() {
-	fmt.Println("Hello world")
+	list, err := Genres()
+	if err != nil {
+		log.Fatal("There was a problem:", err)
+	}
+
+	fmt.Println("Number of Spotify Genres:", len(list))
+}
+
+func Genres() ([]string, error) {
+	if len(genres) > 0 {
+		return genres, nil
+	}
+
+	scraper := colly.NewCollector()
+	scraper.OnHTML("div[preview_url]", func(element *colly.HTMLElement) {
+		genre := strings.TrimSuffix(strings.TrimSpace(element.Text), "»")
+		// Add genre to slice
+		genres = append(genres, genre)
+	})
+
+	err := scraper.Visit("http://everynoise.com/engenremap.html")
+	if err != nil {
+		return nil, err
+	}
+
+	return genres, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/fgoyer/every-genre
 
 go 1.15
+
+require (
+        github.com/gocolly/colly/v2 v2.1.0
+)
